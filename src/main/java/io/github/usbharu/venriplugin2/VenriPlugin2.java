fee7564b2dbdc716f6438c6f45c68a00d68d1481
usbharu <64310155+usbharu@users.noreply.github.com>
2022-08-15 08:08:29 +0900

    一から作り直し

---
+++ src/main/java/io/github/usbharu/venriplugin2/VenriPlugin2.java
@@ -0,0 +1,17 @@
+package io.github.usbharu.venriplugin2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VenriPlugin2 extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
